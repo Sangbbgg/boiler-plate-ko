f88c76dce06a43166fa2d768b30ef78134d1d06c
Sangbbgg <151603886+Sangbbgg@users.noreply.github.com>
2023-12-31 21:55:54 +0900

    login token생성

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
 const { User } = require("./models/User");
+const cookieParser = require("cookie-parser");
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const config = require("./config/key");
 
@@ -17,7 +19,7 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send(`Hell'o World!123asdfasf`));
+app.get("/", (req, res) => res.send(`Hell'o World!`));
 
 app.post("/register", async (req, res) => {
   try {
@@ -28,6 +30,42 @@ app.post("/register", async (req, res) => {
     });
   } catch (err) {
     return res.json({ success: false, err });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    // Find the user with the provided email
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found",
+      });
+
+    // Compare the provided password with the stored hashed password
+    const isMatch = await user.comparePassword(req.body.password);
+
+    if (!isMatch)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, wrong password",
+      });
+
+    // Generate a token and store it in cookies
+    await user.generateToken();
+    
+    res
+      .cookie("x_auth", user.token)
+      .status(200)
+      .json({
+        loginSuccess: true,
+        userId: user._id,
+      });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ loginSuccess: false, message: "Internal Server Error" });
   }
 });
 
--- models/User.js
+++ models/User.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const jwt = require("jsonwebtoken");
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -8,7 +13,7 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     trim: true,
-    unipue: 1,
+    unique: true,
   },
   password: {
     type: String,
@@ -20,7 +25,7 @@ const userSchema = mongoose.Schema({
   },
   role: {
     type: Number,
-    deault: 0,
+    default: 0,
   },
   image: String,
   token: {
@@ -30,6 +35,51 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  var user = this;
+  if (user.isModified("password")) {
+    //비밀번호 암호화
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        (user.password = hash), next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = async function (plainPassword) {
+  try {
+    // Compare the plain password with the hashed password
+    const isMatch = await bcrypt.compare(plainPassword, this.password);
+    return isMatch;
+  } catch (error) {
+    throw error;
+  }
+};
+
+userSchema.methods.generateToken = async function () {
+  try {
+    const user = this;
+
+    // Generate a token using jsonwebtoken
+    const token = jwt.sign(user._id.toHexString(), "yourSecretKey");
+
+    // Save the token in the user object
+    user.token = token;
+
+    // Save the user object with the token in the database
+    await user.save();
+    
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = { User };
